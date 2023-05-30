38a488b4050c2516d2680f4afc267e1d67d0549d
Alexandre de Paula <contatoaledepaula@gmail.com>
2023-05-30 15:57:27 -0300

    apply: search specific soundtrack by id

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const soundtrack = require('./api/soundtracks.json')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const soundtrack = require('./api/soundtracks.json');
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200,
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
 };
 
-app.get('/soundtrack', (req, res) => {
-    res.json(soundtrack)
-})
+const tracks = soundtrack;
 
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-    res.status(404).json({ error: 'Rota não encontrada' })
-})
+app.get('/soundtrack', (req, res) => {
+  res.json(tracks);
+});
 
-app.listen(5000, () => {
-    console.log('Servidor rodando na porta 5000')
-})
+app.get('/soundtrack/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const track = tracks.find(track => track.id === id);
+  if (track) {
+    res.json(track);
+  } else {
+    res.status(404).json({ error: 'Trilha sonora não encontrada.' });
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Rota não encontrada' });
+});
+
+app.listen(8080, () => {
+  console.log('Servidor rodando na porta 8080');
+});
